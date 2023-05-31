49a6b5e2792260f75f0a1e3bd71b7df0fca4b1b8
yoke-ching <yoke-ching.mah@keysight.com>
2023-05-31 08:00:05 +0000

    correct

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("BoringDate");
+        System.out.println("today is wednesday");
     }
 
     public String getMessage() {
