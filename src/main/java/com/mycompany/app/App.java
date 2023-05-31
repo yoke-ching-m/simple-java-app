c565f065f766326de3933e86e66b61747d03b65e
yoke-ching <yoke-ching.mah@keysight.com>
2023-05-31 07:30:59 +0000

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Today is hot");
+        System.out.println("BoringDate");
     }
 
     public String getMessage() {
