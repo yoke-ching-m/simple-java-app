c37ddfaebf78e099a130119ce5f55d491effffbb
yoke-ching <yoke-ching.mah@keysight.com>
2023-06-01 05:29:55 +0000

    simply change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("today is wednesday and it is hot... 5pm soon...:D");
+        System.out.println("today is thursday :D ");
     }
 
     public String getMessage() {
